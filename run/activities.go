f80b67d8d8ac83c1d305e072d6bcf11e31cdd81e
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-01-03 23:31:59 +0700

    Fix git activites

--- run/activities.go
+++ run/activities.go
@@ -1,17 +1,36 @@
 package run
 
-import "github.com/globalsign/mgo/bson"
+import (
+	"time"
+
+	"github.com/globalsign/mgo/bson"
+)
 
 func (v *VirtualRun) AthleteActivities(id uint32, period []string) ([]Activity, error) {
 	activities := []Activity{}
 
-	e := v.db.List("activities", bson.M{
+	from, e := time.Parse(time.RFC3339, period[0])
+	if e != nil {
+		return nil, e
+	}
+	fromLocation := from.Location()
+	newForm := time.Date(from.Year(), from.Month(), from.Day(), 0, 0, 0, 0, fromLocation)
+	fromStamp := newForm.Format(time.RFC3339)
+	to, e := time.Parse(time.RFC3339, period[1])
+	if e != nil {
+		return nil, e
+	}
+	toLocation := to.Location()
+	newTo := time.Date(to.Year(), to.Month(), to.Day(), 23, 59, 59, 999, toLocation)
+	toStamp := newTo.Format(time.RFC3339)
+
+	e = v.db.List("activities", bson.M{
 		"athlete": bson.M{
 			"id": id,
 		},
 		"startdate": bson.M{
-			"$gte": period[0],
-			"$lte": period[1],
+			"$gte": fromStamp,
+			"$lte": toStamp,
 		},
 	}, []string{"-startdate"}, &activities)
 
